6e9bfc9b8ac51f38bf3298126aa12ee6b53eed6b
HUY LE <ongconoizzz.com>
2021-10-26 20:52:41 +0700

    add eat to die for Mouse

--- Cat.js
+++ Cat.js
@@ -1,4 +1,17 @@
+var Mouse = require("./Mouse");
+
 function Cat() {
-    this.stomach = [];
+  this.stomach = [];
 }
-module.exports =Cat;
+
+Cat.prototype.eat = function (mouse) {
+  this.stomach.push(mouse);
+  mouse.die();
+};
+
+module.exports = Cat;
+
+var mickey = new Mouse("mickey");
+var tom = new Cat("tom");
+tom.eat(mickey);
+console.log(tom);
