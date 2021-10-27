e7612af11b2befadab4f4678dfffde789522acda
HUY LE <ongconoizzz.com>
2021-10-27 21:50:14 +0700

    Dog eat Cat

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
- //TODO Học hàm setTimeout và clearTimeout
+//TODO Học hàm setTimeout và clearTimeout
 //!----> setTimeout sẽ làm một hàm gì đó sau khoảng thời gian quy định
 //*syntax setTimeout(fn, ms) // milliseconds
 //?có thể tạo hàm ở ngoài rồi truyền vô
 var done = function () {
-    console.log("FINISH");
-  };
+  console.log("FINISH");
+};
 console.log("Start");
 //!bên dưới là truyền thẳng hàm vào bên trong
 // var timeoutId = setTimeout(function () {
@@ -15,8 +15,10 @@ var timeoutId = setTimeout(done, 2000);
 // clearTimeout(timeoutId);//*xóa timeout
 console.log("END");
 
-var Dog = require('./Dog');
+var Dog = require("./Dog");
+var Cat = require("./Cat");
 
-var dog = new Dog('Tom');
-dog.sayHi();
-
+var dog = new Dog("Tom");
+var tom = new Cat();
+dog.eat(tom);
+console.log(dog);
